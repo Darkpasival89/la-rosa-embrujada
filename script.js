1e128576a0ce502da48f3fab47c25740875f1e4b
darkpasival89 <cinechispa@gmail.com>
2024-10-17 23:15:00 +0200

    Commit message:nuevo estilo

---
+++ script.js
@@ -0,0 +1,18 @@
+// Event listener para cuando la página esté lista
+document.addEventListener("DOMContentLoaded", function() {
+  const flower = document.querySelector(".flower");
+  const fallingPetal = document.querySelector(".falling-petal");
+
+  // Verifica si los elementos existen
+  if (!flower || !fallingPetal) {
+    console.error("Los elementos .flower o .falling-petal no se encontraron.");
+    return;
+  }
+
+  let previousTitle;
+
+  // Reiniciar la animación del pétalo que cae
+  fallingPetal.addEventListener('animationiteration', () => {
+    fallingPetal.style.left = `${Math.random() * 80 + 10}vw`;
+  });
+});
